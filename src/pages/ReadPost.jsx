ce19fd35fc55280909811b5063a757221f058cb2
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-26 18:41:13 -0500

    created page to display posts

---
+++ src/pages/ReadPost.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect} from "react";
+import { supabase } from "/Users/jerin/Programming/Codepath/Github/Forum/client.js";
+import Card from "../components/Card";
+
+const ReadPost = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const {data} = await supabase
+                .from('Forum')
+                .select()
+                .order('created_at', {ascending: true})
+            setPosts(data);
+        }
+        fetchPost();
+    },[])
+
+    return (
+        <div className="ReadPost">
+            {
+                posts && posts.length > 0 ?
+                posts.map((post,index) => 
+                    <Card id={post.id} title={post.title} key={index}/>
+                ) : <h2>{'No Posts to see yet! :( '}</h2>
+            }
+        </div>
+    )
+}
+
+export default ReadPost;
