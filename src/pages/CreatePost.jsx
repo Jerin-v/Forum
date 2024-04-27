2deeeaaa70cd172d1ddb9b9c898b161f44112c2b
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-26 19:45:49 -0500

    add function to create post and handle change

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -4,9 +4,30 @@ import { supabase } from "../../client";
 const CreatePost = () => {
     const [post, setPost] = useState({title: "", author: "", content: ""})
 
+    const createPost = async (event) => {
+        event.preventDefault();
+
+        await supabase
+            .from('Forum')
+            .insert({title: post.title, author: post.author, content: post.content})
+            .select()
+
+            window.location = "/"
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
     return (
         <div>
-            
+
         </div>
     )
 }
