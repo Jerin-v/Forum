b637a338b9a6c8943fb0431f99a195e3a7822a52
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-26 19:43:43 -0500

    Add CreatePost component

---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { supabase } from "../../client";
+
+const CreatePost = () => {
+    const [post, setPost] = useState({title: "", author: "", content: ""})
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
